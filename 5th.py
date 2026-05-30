2f6bcd506f90fc7423204253cf31d2844e14e59b
Swagbaa <s_gelashvili8@cu.edu.ge>
2026-05-30 23:30:23 +0400

    2nd task

--- 5th.py
+++ 5th.py
@@ -1,16 +1,30 @@
 #N1
-from itertools import permutations as perm
 
-def possibilities(text):
-    perms=[''.join(p) for p in perm(text)]
+# from itertools import permutations as perm
+#
+# def possibilities(text):
+#     perms=[''.join(p) for p in perm(text)]
+#
+#     total=[]
+#     for p in perms:
+#         total.append(p)
+#
+#     return len(total), total
+#
+#
+# while True:
+#     user_input=input('Enter text: ')
+#     print(possibilities(user_input))
 
-    total=[]
-    for p in perms:
-        total.append(p)
+# N2
 
-    return len(total), total
+from datetime import datetime, timedelta
 
+today=datetime.today()
 
-while True:
-    user_input=input('Enter text: ')
-    print(possibilities(user_input))
+samshabati=1
+samshabatamde=(samshabati-today.weekday())%7
+
+shemdegi=today+timedelta(days=samshabatamde)
+
+print(shemdegi.date())
